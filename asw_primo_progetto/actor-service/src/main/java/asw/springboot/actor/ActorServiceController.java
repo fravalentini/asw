9070dc7dbffc56d317bad50e8e05c13e711ac720
fravalentini <francesco.valentini87@gmail.com>
2017-06-05 16:10:27 +0200

    Added uris in property file

--- asw_primo_progetto/actor-service/src/main/java/asw/springboot/actor/ActorServiceController.java
+++ asw_primo_progetto/actor-service/src/main/java/asw/springboot/actor/ActorServiceController.java
@@ -2,6 +2,7 @@ package asw.springboot.actor;
 
 import java.util.logging.Logger;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,11 +13,17 @@ public class ActorServiceController {
 	
 	private final Logger logger = Logger.getLogger("asw.springboot.actor");
 	
+	@Value("${uri.nationality}")
+	private String uriNat;
+	
+	@Value("${uri.award}")
+	private String uriAw;
+	
 	@RequestMapping("/S/{actor}")
 	public String getActorInfo (@PathVariable String actor){
 		RestTemplate restTemplate = new RestTemplate(); 
-		String nationality = restTemplate.getForObject("http://localhost:8081/S1/" + actor, String.class);
-		String award = restTemplate.getForObject("http://localhost:8082/S2/" + actor, String.class);
+		String nationality = restTemplate.getForObject(uriNat + actor, String.class);
+		String award = restTemplate.getForObject(uriAw + actor, String.class);
 		String io = "i";
 		if (Integer.parseInt(award) == 1) io += "o";
 		String s = actor + " e' un attore " + nationality + " e ha vinto " + award + " prem" + io + " nella sua carriera";
@@ -27,8 +34,8 @@ public class ActorServiceController {
 	@RequestMapping("/S/{actor}/{year}")
 	public String getActorInfo (@PathVariable String actor, @PathVariable String year){
 		RestTemplate restTemplate = new RestTemplate(); 
-		String nationality = restTemplate.getForObject("http://localhost:8081/S1/" + actor, String.class);
-		String award = restTemplate.getForObject("http://localhost:8082/S2/" + actor + "/" + year, String.class);
+		String nationality = restTemplate.getForObject(uriNat + actor, String.class);
+		String award = restTemplate.getForObject(uriAw + actor + "/" + year, String.class);
 		String io = "i";
 		if (Integer.parseInt(award) == 1) io += "o";
 		String s = actor + " e' un attore " + nationality + " e ha vinto " + award + " prem" + io + " nell'anno " + year;
